10cc28c1c322f2feadf5ad0a5dd7172f10cc8e1b
Tadashi Yamazaki <tada4y1012@outlook.com>
2019-11-16 19:06:13 +0900

    3rd commit

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({
+    connectionString
+});
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('hello');
+    pool.query('SELECT * FROM users', (err, resp) => {
+        if (err) {
+            res.status(500).send('internal error');
+        } else {
+            res.send(JSON.stringify(resp.rows));
+        }
+    });
 });
 
 app.listen(port, () => {
